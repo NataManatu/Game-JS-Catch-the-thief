6e342dd304044ce2fccd9d3afddab289183273cb
NataR78 <100790826+NataR78@users.noreply.github.com>
2022-08-01 00:59:43 +0300

    пой вор

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ let loops = 0;
 let peopleVisible = false
 
 function gameLoop() {
-    peopleVisible = !peopleVisible
+    peopleVisible = !peopleVisible;
+    flashCharacters();
     loops++;
     if (loops < 12) {
         setTimeout(gameLoop, 3000);
@@ -22,5 +23,14 @@ function gameLoop() {
 function flashCharacters() {
     let board = document.getElementById("board");
     let classToSet = "";
+    if (peopleVisible) {
+        classToSet = "character visible";
+    }
+    else {
+        classToSet = "character hidden ";
+    }
+    for (let index = 0; index < 6; index++) {
+        board.children[index].className = classToSet;
 
+    }
 }
