6c72629aaf1c4b75d4e50a448aefa6c5ae362990
NataR78 <100790826+NataR78@users.noreply.github.com>
2022-08-01 00:04:01 +0300

    поймай вора

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let buttonStart = document.getElementById("button-start")
 
 function startGame() {
-
+    gameLoop();
 }
 buttonStart.addEventListener("click", startGame)
+function gameLoop() {
+    alert("game over!");
+    setTimeout(gameLoop, 3000);
+}
